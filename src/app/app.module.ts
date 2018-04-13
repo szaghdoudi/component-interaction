82a2e7267091ddde6553699c69101dd85e84a605
szaghdoudi <sofian.zaghdoudi@gmail.com>
2018-04-13 12:48:44 +0100

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { VersionChildComponent } from './version-child/version-child.component';
+import { VersionParentComponent } from './version-parent/version-parent.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VersionChildComponent,
+    VersionParentComponent
   ],
   imports: [
     BrowserModule
